34a7d0ac9b39f953e9680b41291c91ee0915d6ec
abhi67449 <abhi67449@gmail.com>
2025-02-15 18:33:22 +0530

    Geek Food

--- GeekFood/src/Componenets/BodyComponent/Body.jsx
+++ GeekFood/src/Componenets/BodyComponent/Body.jsx
@@ -5,7 +5,7 @@ function Body() {
   return (
     <div>
       <div className='image1'>
-        <img src="https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="" />
+        {/* <img src="https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="" /> */}
       </div>
     </div>
   )
