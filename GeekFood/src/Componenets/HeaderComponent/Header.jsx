7bc47bf7d530c6700b97d180f6b6a8379abd919a
abhi67449 <abhi67449@gmail.com>
2025-02-18 01:34:16 +0530

    Geek Food

--- GeekFood/src/Componenets/HeaderComponent/Header.jsx
+++ GeekFood/src/Componenets/HeaderComponent/Header.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import './Header.css'
+import logo from "../BodyComponent/logo.svg"
 
 function Header() {
+
   return (
     <div className='nav'>
       <div className='logo-nav'>
-        <span style={{fontWeight:"bolder",cursor:"pointer"}}>GeekFoods</span>
+        <img className='logo-image' src={logo}></img>
+        <span style={{fontWeight:"bolder",cursor:"pointer",marginLeft:"10px",fontSize:"25px"}}>GeekFoods</span>
       </div>
       <div className='main-nav'>
             <ul className='list'>
-                <li>Home</li>
-                <li>Quote</li>
-                <li>Restaurant</li>
-                <li>Food</li>
-                <li>Contact</li>
+                <li className='list-header' style={{color:"blue"}}>Home</li>
+                <li className='list-header'>Quote</li>
+                <li className='list-header'>Restaurant</li>
+                <li className='list-header'>Food</li>
+                <li className='list-header'>Contact</li>
             </ul>
       </div>
       <div className='button'>
